6ae379754b05b6488d1ea1d19fed4b8d9f0a2ccc
BonePolk <87945722+BonePolk@users.noreply.github.com>
2023-08-09 13:37:56 +0500

    pep errors fixing

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from random import *
-from parser import *
-from time import sleep
+from random import randint
 
-while True:
-	while True:
-	   try:
-	       page = randint(1,parserpages("mods"))
-	       print("Страница N°", page, sep="")
-	       print(*parser(randint(0,9), page, "mods"), sep = "")
-	       print("следующий введите Enter")
-	       input()
-	       break
-	   except Exception as ex:
-	       print(ex)
+from parser import parser
+
+def main():
+    while True:
+        while True:
+            try:
+                page = randint(1, parser.parserpages("mods"))
+                print("Страница N°", page, sep="")
+                print(*parser.parser(randint(0, 9), page, "mods"), sep="")
+                print("Следующий введите Enter")
+                input()
+                break
+            except Exception as ex:
+                print(ex)
+
+if __name__ == "__main__":
+    main()
